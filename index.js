7f312217f0cd188cee69bb11d54117429fd134af
jakep5 <jakepagel1@yahoo.com>
2019-10-03 16:56:52 -0500

    add GET function and begin DOM manipulation

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const apiKey = '0590ac6e3b8a2d55f8589c81ddb2ac44';
-const baseUrl = 'https://api.musixmatch.com/ws/1.1/'
+const myApiKey = '9c926ea14e70b0a8a01451f9b1e9a4c2';
+const baseUrl = 'https://api.audd.io/findLyrics/'
 
+
+/* EVENT LISTENERS */
+/* function watchButton () {
+    $("button.submitButton").on('click', function() {
+        $("form").submit();
+    })
+} */
 function watchSubmit (){
     $("form").submit(function(e) {
         e.preventDefault();
+        const snippet = $("#songLyric").val();
+        getLyrics(snippet);
     }
     )};
 
@@ -13,23 +22,81 @@ function watchFilter () {
     });
 }
 
+function watchFilterSelect() {
+
+}
+//
+
+/* CONSTRUCT URL */
+function constUrl (params) {
+    const queryItems = Object.keys(params)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&'); //==>queryItems.join == queryString
+}
+//
+
+/* GET FUNCTION */
+function getLyrics(snippet) {
+    const params = {
+        q: snippet,
+        /* s_artist_rating: 'DESC', */
+        api_token: myApiKey,
+        /* format:"jsonp", */
+    }
+    const queryString = constUrl(params)
+    const url = baseUrl + '?' + queryString;
+    console.log(url);
+    fetch (url)
+        .then (response => {
+            if (response.ok) {
+                return response.json();
+            }
+            else {
+                throw new Error(response.statusText);
+            }
+            })
+        .then (responseJson => generateResults(responseJson))
+       /*  .then (responseJson => createLyricsSnippet(responseJson)) */
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        })
+    };
+//
+
+/* LYRICS SNIPPET CREATOR */
+/* function createLyricsSnippet(responseJson) {
+    const lyricArray = [];
+    for (let i=0; i<(responseJson.result).length;i++){
+
+
+    }
+} */
+/* MANIPULATE DOM */
+function generateResults(responseJson, lyricArray) {
+    console.log(responseJson);
+    $("div.resultsHolder").empty();
+    $("div.searchBarHolder").toggleClass("hidden");
+    for (let i = 0; i<(responseJson.result).length;i++) {
+        $("div.resultsHolder").append(`
+            <div class="resultItem">
+                <h1 class="songTitleHeader">Song title: ${responseJson.result[i].full_title}</h1>
+                <button class="resultsButton spotify">Spotify</button>
+                <button class="resultsButton youtube">YouTube</button>
+                <button class="resultsButton wiki">Artist Wiki</button>
+            </div>
+        `)
+}
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
+}
 
 $(function () {
+  /*   watchButton(); */
     watchSubmit();
     watchFilter();
+    watchFilterSelect();
+    console.log('Ready! Waiting for submit.');
 })  
