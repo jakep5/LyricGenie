408dc22a51b2720faa05f5f48f5c5ccb99bb6ddc
jakep5 <jakepagel1@yahoo.com>
2019-10-03 14:44:57 -0500

    add filter display, begin query parameters

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const apiKey = '0590ac6e3b8a2d55f8589c81ddb2ac44';
+const baseUrl = 'https://api.musixmatch.com/ws/1.1/'
 
+function watchSubmit (){
+    $("form").submit(function(e) {
+        e.preventDefault();
+    }
+    )};
+
+function watchFilter () {
+    $("button.filterButton").on('click', function(){
+        $("div.filterDisplay").toggleClass("hidden");
+    });
+}
 
 
 
@@ -19,4 +32,4 @@
 $(function () {
     watchSubmit();
     watchFilter();
-})
+})  
