48c9079b0063e757b9d083bd9b85f75b023a7510
jakep5 <jakepagel1@yahoo.com>
2019-10-08 14:46:10 -0500

    stylize/resize YouTube link icon

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ function generateResults(lyricObject, snippetArray, linkArray, snippet) {
             $("div.resultsHolder").append(`
                 <div class="resultItem">
                     <h1 class="songTitleHeader">${lyricObject.result[i].full_title}</h1>
-                    <a href=${linkArray[i]} target="_blank" class="youTube"><i class="fab fa-youtube-square"></i></a>
+                    <a href=${linkArray[i]} target="_blank" class="youTube"><img src="https://www.freepnglogos.com/uploads/youtube-vector-logo-png-9.png" class="youTubeImage" alt="YouTube Link"></a>
                     <h2 class="songSnippet">"...${snippetArray[i]}..."</h2>
                 </div>
             `)
@@ -165,33 +165,24 @@ function generateResults(lyricObject, snippetArray, linkArray, snippet) {
 /* BUBBLE EFFECT */
 jQuery(document).ready(function($){
  
-    // Define a blank array for the effect positions. This will be populated based on width of the title.
     var bArray = [];
-    // Define a size array, this will be used to vary bubble sizes
     var sArray = [4,6,8,10];
  
-    // Push the header width values to bArray
     for (var i = 0; i < $('.bubbles').width(); i++) {
         bArray.push(i);
     }
      
-    // Function to select random array element
-    // Used within the setInterval a few times
     function randomValue(arr) {
         return arr[Math.floor(Math.random() * arr.length)];
     }
  
-    // setInterval function used to create new bubble every 350 milliseconds
     setInterval(function(){
          
-        // Get a random size, defined as variable so it can be used for both width and height
         var size = randomValue(sArray);
-        // New bubble appeneded to div with it's size and left position being set inline
-        // Left value is set through getting a random value from bArray
+    
         $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
          
-        // Animate each bubble to the top (bottom 100%) and reduce opacity as it moves
-        // Callback function used to remove finsihed animations from the page
+        
         $('.individual-bubble').animate({
             'bottom': '100%',
             'opacity' : '-=0.7'
